7d3f64fbb9df2ce2e956e9a94f53894864f30ea1
Maksim <camenzindmp@gmail.com>
2020-02-25 22:56:52 +1000

    version 2(final)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int creditAmount = 1_000_000; // Сумма кредита;
-        double yearPercent = 9.99; // Годовой процент;
-        int maturity = 12; // Срок выплаты кредита;
-        double monthlyPayment = service.calculate(creditAmount, yearPercent, maturity);
-        System.out.println(monthlyPayment);
+
+        double monthlyPayment12 = service.calculate(1_000_000, 9.99, 12);
+        int resultFor12 = (int)monthlyPayment12;
+        System.out.println("Ежемесячный платеж " + resultFor12);
+
+        double monthlyPayment24 = service.calculate(1_000_000, 9.99, 24);
+        int resultFor24 = (int)monthlyPayment24;
+        System.out.println("Ежемесячный платеж " + resultFor24);
+
+        double monthlyPayment36 = service.calculate(1_000_000, 9.99, 36);
+        int resultFor36 = (int)monthlyPayment36;
+        System.out.println("Ежемесячный платеж " + resultFor36);
     }
 }
